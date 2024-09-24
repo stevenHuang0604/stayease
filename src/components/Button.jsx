a34c07617415d6d32f0cc254dbd54eec284fd1de
Steven Huang <sarsky228@gmail.com>
2024-09-24 22:07:11 +0800

    feat(ui): Add versatile Button components
    - Implement Button component with support for regular and Link usage
    - Add 4 variants: default, outline, ghost, link
    - Include 4 color schemes: primary, secondary, success, danger
    - Support 4 sizes: small, medium, large, xl
    - Ensure component is fully typed and accessible

---
+++ src/components/Button.jsx
@@ -0,0 +1,108 @@
+import { Link } from "react-router-dom";
+
+function Button({
+  variant = "default",
+  color = "primary",
+  size = "medium",
+  to,
+  onClick,
+  children,
+}) {
+  let className =
+    "flex items-center rounded-lg font-semibold  transition-all duration-200 ";
+
+  const variants = {
+    default: "",
+    outline: "bg-transparent outline outline-1",
+    ghost: "bg-transparent hover:bg-opacity-10",
+    link: "bg-transparent underline hover:bg-opacity-10",
+  };
+  className += variants[variant] + " ";
+
+  // 顏色樣式
+  const colors = {
+    primary: {
+      default: "bg-violet-600 text-slate-50 hover:bg-violet-800",
+      outline:
+        "outline-violet-600 text-violet-600 hover:outline-violet-800 hover:text-violet-800 hover:bg-violet-200",
+      ghost: "text-violet-600 hover:bg-violet-600",
+      link: "text-violet-600 hover:text-violet-800",
+    },
+    secondary: {
+      default: "bg-teal-600 text-slate-50",
+      outline:
+        "outline-teal-600 text-teal-600 hover:outline-teal-600 hover:text-teal-800 hover:bg-teal-200",
+      ghost: "text-teal-600 hover:bg-teal-600",
+      link: "text-teal-600 hover:text-teal-800",
+    },
+    success: {
+      default: "bg-green-600 text-slate-50",
+      outline:
+        "outline-green-600 text-green-600 hover:outline-green-600 hover:text-green-800 hover:bg-green-200",
+      ghost: "text-green-600 hover:bg-green-600",
+      link: "text-green-600 hover:text-green-800",
+    },
+    danger: {
+      default: "bg-red-600 text-slate-50",
+      outline:
+        "outline-red-600 text-red-600 hover:outline-red-600 hover:text-red-800 hover:bg-red-200",
+      ghost: "text-red-600 hover:bg-red-600",
+      link: "text-red-600 hover:text-red-800",
+    },
+  };
+  className += colors[color][variant] + " ";
+
+  // 尺寸樣式
+  const sizes = {
+    small: "px-2 py-1 text-sm shadow-sm",
+    medium: "px-3 py-1.5 text-base shadow",
+    large: "px-4 py-2 text-lg shadow-lg",
+    xl: "px-6 py-3 text-xl shadow-xl",
+  };
+  className += sizes[size] + " ";
+
+  // switch (type) {
+  //   case "primary":
+  //     className += "bg-violet-600 text-slate-50 ";
+  //     break;
+  //   default:
+  //     className += "bg-slate-50 text--violet-600 ";
+  // }
+
+  // switch (size) {
+  //   case "small":
+  //   case "medium":
+  //     className += "py-1.5 px-2.5 ";
+  //     break;
+  //   case "large":
+  //   default:
+  // }
+
+  // if (style === "outline") {
+  //   className += "outline outline-1 outline-violet-600 text-violet-600 ";
+  // }
+
+  // if (to) {
+  //   return (
+  //     <Link to={to} className={className}>
+  //       {children}
+  //     </Link>
+  //   );
+  // }
+
+  // return (
+  //   <button className={className} onClick={onClick}>
+  //     {children}
+  //   </button>
+  // );
+
+  const Element = to ? Link : "button";
+
+  return (
+    <Element to={to} className={className} onClick={onClick}>
+      {children}
+    </Element>
+  );
+}
+
+export default Button;
--- src/homepages/NavigationSection/Navigation.jsx
+++ src/homepages/NavigationSection/Navigation.jsx
@@ -1,5 +1,25 @@
+import { FaRegMoon } from "react-icons/fa6";
+import Button from "../../components/Button";
+
 function Navigation() {
-  return <div>Nav</div>;
+  return (
+    <nav className="mx-auto max-w-full">
+      <div className="flex h-32 items-center px-12 py-6">
+        <img src="/logo-light.svg" alt="StayEase logo" className="h-full" />
+        <div className="ml-auto flex items-center gap-4 text-lg">
+          <Button variant="default" color="primary" size="medium" to="./signin">
+            SignIn
+          </Button>
+          <Button variant="outline" color="primary" size="medium" to="/signup">
+            SignUp
+          </Button>
+          <Button variant="ghost" color="primary" size="large">
+            <FaRegMoon />
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
