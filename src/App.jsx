66903dc9e66497339f02cacda48377f141f630b6
Steven Huang <sarsky228@gmail.com>
2024-10-03 14:33:17 +0800

    style: styling reservation detail page

--- src/App.jsx
+++ src/App.jsx
@@ -93,8 +93,8 @@ function App() {
             fontSize: "16px",
             maxWidth: "500px",
             padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
+            backgroundColor: "#fff",
+            color: "#374151",
           },
         }}
       />
--- src/features/reservations/ReservationDetail.jsx
+++ src/features/reservations/ReservationDetail.jsx
@@ -19,72 +19,76 @@ function ReservationDetail() {
   const hotel = hotels[0];
 
   return (
-    <div className="mx-auto flex w-[80%] flex-col gap-8">
-      <div className="overflow-hidden rounded-lg shadow-md">
-        <div className="flex items-center justify-between bg-violet-500 px-10 py-5 text-lg font-medium text-slate-200">
-          <div className="flex items-center gap-4 text-xl font-semibold">
-            <HiOutlineHomeModern className="h-8 w-8" />
-            <p>10 nights in {hotel.name}</p>
+    <main className="px-14 py-14 md:px-16 md:py-16">
+      <div className="mx-auto flex w-[80%] flex-col gap-8">
+        <div className="overflow-hidden rounded-lg shadow-md">
+          <div className="flex items-center justify-between bg-violet-500 px-10 py-5 text-lg font-medium text-slate-200">
+            <div className="flex items-center gap-4 text-xl font-semibold">
+              <HiOutlineHomeModern className="h-8 w-8" />
+              <p>10 nights in {hotel.name}</p>
+            </div>
+
+            <p>
+              {formatDate(new Date(reservation.check_in_date).toISOString())}{" "}
+              &mdash;{" "}
+              {formatDate(new Date(reservation.check_out_date).toISOString())}
+            </p>
           </div>
 
-          <p>
-            {formatDate(new Date(reservation.check_in_date).toISOString())}{" "}
-            &mdash;{" "}
-            {formatDate(new Date(reservation.check_out_date).toISOString())}
-          </p>
+          <section className="px-10 pb-3 pt-8">
+            <div className="flex items-center gap-4 py-2 text-base text-blue-600">
+              <FaLocationDot className="flex h-5 w-5 items-center justify-center" />
+              <span>{`${hotel.city}, ${hotel.country}`}</span>
+            </div>
+
+            <div className="mb-4 flex items-center justify-between">
+              <h1 className="text-xl font-medium">{hotel.name}</h1>
+            </div>
+
+            <div className="flex items-center gap-4 py-2 text-lg font-medium">
+              <HiOutlineUsers className="h-5 w-5 text-violet-500" />
+              <p>{reservation.guests} guests</p>
+            </div>
+
+            <div className="font-base flex items-center gap-4 py-2 text-lg font-medium">
+              <HiOutlineBuildingStorefront className="h-5 w-5 text-violet-500" />
+              <p>{reservation.rooms} rooms</p>
+            </div>
+
+            <div className="mb-auto mt-5 flex items-center gap-4 pb-4 text-base text-zinc-600">
+              <FaLocationArrow className="h-5 w-5" />
+              <span className="border-b border-b-slate-800">
+                {hotel.address}
+              </span>
+            </div>
+
+            <div className="mt-6 rounded bg-green-100 px-8 py-4 text-green-700">
+              <div className="flex items-center gap-4">
+                <span className="flex items-center gap-2 font-medium">
+                  <HiOutlineCurrencyDollar className="h-6 w-6" />
+                  Total price
+                </span>
+                ${hotel.base_price * reservation.rooms} (${hotel.base_price}{" "}
+                each for {reservation.rooms} rooms)
+              </div>
+            </div>
+          </section>
+
+          <footer className="px-10 py-4 text-right text-xs text-slate-400">
+            <p>Booked {formatTime(reservation.createdAt)}</p>
+          </footer>
         </div>
 
-        <section className="px-10 pb-3 pt-8">
-          <div className="flex items-center gap-4 py-2 text-base text-blue-600">
-            <FaLocationDot className="flex h-5 w-5 items-center justify-center" />
-            <span>{`${hotel.city}, ${hotel.country}`}</span>
-          </div>
-
-          <div className="mb-4 flex items-center justify-between">
-            <h1 className="text-xl font-medium">{hotel.name}</h1>
-          </div>
-
-          <div className="flex items-center gap-4 py-2 text-lg font-medium">
-            <HiOutlineUsers className="h-5 w-5 text-violet-500" />
-            <p>{reservation.guests} guests</p>
-          </div>
-
-          <div className="font-base flex items-center gap-4 py-2 text-lg font-medium">
-            <HiOutlineBuildingStorefront className="h-5 w-5 text-violet-500" />
-            <p>{reservation.rooms} rooms</p>
-          </div>
-
-          <div className="mb-auto mt-5 flex items-center gap-4 pb-4 text-base text-zinc-600">
-            <FaLocationArrow className="h-5 w-5" />
-            <span className="border-b border-b-slate-800">{hotel.address}</span>
-          </div>
-
-          <div className="mt-6 rounded bg-green-100 px-8 py-4 text-green-700">
-            <div className="flex items-center gap-4">
-              <span className="flex items-center gap-2 font-medium">
-                <HiOutlineCurrencyDollar className="h-6 w-6" />
-                Total price
-              </span>
-              ${hotel.base_price * reservation.rooms} (${hotel.base_price} each
-              for {reservation.rooms} rooms)
-            </div>
-          </div>
-        </section>
-
-        <footer className="px-10 py-4 text-right text-xs text-slate-400">
-          <p>Booked {formatTime(reservation.createdAt)}</p>
-        </footer>
+        <div className="ml-auto">
+          <Link
+            to={`/app/hotels/${hotel.id}`}
+            className="rounded-md bg-violet-600 px-4 py-3 text-right font-medium text-slate-50"
+          >
+            Hotel details
+          </Link>
+        </div>
       </div>
-
-      <div className="ml-auto">
-        <Link
-          to={`/app/hotels/${hotel.id}`}
-          className="rounded-md bg-violet-600 px-4 py-3 text-right font-medium text-slate-50"
-        >
-          Hotel details
-        </Link>
-      </div>
-    </div>
+    </main>
   );
 }
 
