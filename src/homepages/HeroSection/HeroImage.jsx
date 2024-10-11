b05377f4fba6f3404c099823b750dbded3208cb5
Steven Huang <sarsky228@gmail.com>
2024-10-11 17:09:46 +0800

    style: center the Spinner in HeroImage.jsx

--- src/homepages/HeroSection/HeroImage.jsx
+++ src/homepages/HeroSection/HeroImage.jsx
@@ -2,8 +2,8 @@ import ReservationsContainer from "../../features/reservations/ReservationsConta
 
 function HeroImage() {
   return (
-    <div className="perspective-1000 flex basis-2/4 scale-90 overflow-hidden opacity-50 lg:absolute lg:left-2/4 lg:top-2/4 lg:translate-y-[-50%] lg:scale-[0.70]">
-      <div className="rotate-3d-[-1,1,-1,-15deg] max-w-full transform">
+    <div className="fc basis-2/4 scale-90 overflow-hidden opacity-50 perspective-1000 lg:absolute lg:left-2/4 lg:top-2/4 lg:translate-y-[-50%] lg:scale-[0.70]">
+      <div className="max-w-full transform rotate-3d-[-1,1,-1,-15deg]">
         <ReservationsContainer page="home" />
       </div>
     </div>
