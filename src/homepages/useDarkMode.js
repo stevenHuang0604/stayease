381afa345f9ab70d94ba35683c373f8d8a1f6eec
Steven Huang <sarsky228@gmail.com>
2024-12-13 10:21:05 +0800

    refactor: simplify darkmode setting

--- src/homepages/useDarkMode.js
+++ src/homepages/useDarkMode.js
@@ -8,33 +8,13 @@ export function useDarkMode() {
       "(prefers-color-scheme: dark)",
     );
 
+    if (darkModeMediaQuery.matches)
+      document.documentElement.classList.add("dark");
+
     setIsDarkMode(
       document.documentElement.classList.contains("dark") ||
         darkModeMediaQuery.matches,
     );
-
-    if (darkModeMediaQuery.matches)
-      document.documentElement.classList.add("dark");
-
-    /*
-    useEffect hook with empty dependencies array runs once after the initial render.
-    The MutationObserver is created and set up.
-    Once set up, the observer continues to run independently of React's lifecycle.
-    The observer persists and continues to monitor the DOM for changes once initialized.
-    The callback function has closure over the setIsDarkMode function, allowing it to update the React state.
-    */
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === "class") {
-          setIsDarkMode(document.documentElement.classList.contains("dark"));
-        }
-      });
-    });
-
-    observer.observe(document.documentElement, { attributes: true });
-
-    // Clean-up function ensures the observer is properly cleaned up when the component unmounts
-    return () => observer.disconnect();
   }, []);
 
   const toggleDarkMode = () => {
